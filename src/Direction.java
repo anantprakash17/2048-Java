6f379dc38677f000b1e461a68608bf642b2533e2
Anant Prakash <anantprakash92@gmail.com>
2021-05-08 23:29:23 -0400

    Implimentation for the Direction module, for possible movement of the board

---
+++ src/Direction.java
@@ -0,0 +1,18 @@
+/*
+ * Author: Anant Prakash
+ * Revised: May 8th 2021
+ *
+ * Description: Enum for directions of board movement.
+ */
+
+package src;
+
+/**
+ * The enum Direction.
+ */
+public enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
